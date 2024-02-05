f8bf4c94fbc81254b792b1c330de8cff1164fc67
Zach Leinweber <zleinweb@gmail.com>
2024-02-04 21:02:33 -0600

    First spike (#1)
    * Start laying out project structure
    
    * Recipe decisions must come from the environment
    
    * Implement installing basic set of packages via Brewfile

---
+++ Brewfile
@@ -0,0 +1,18 @@
+tap "homebrew/cask"
+tap "homebrew/core"
+
+brew "bat"
+brew "coreutils"
+brew "gh"
+brew "grep"
+brew "neovim"
+brew "tmux"
+brew "wget"
+
+cask "amethyst"
+cask "discord"
+cask "docker"
+cask "github"
+cask "iterm2"
+cask "visual-studio-code"
+cask "warp"
---
+++ scripts/homebrew.sh
@@ -0,0 +1,65 @@
+#!/usr/bin/env bash
+
+## homebrew.sh ##
+#
+# Script to run common homebrew tasks. Available commands:
+# - `install` - Install homebrew if it's not already installed
+# - `bundle` - Install packages from a Brewfile bundle
+#
+
+## Globals ##
+HOMEBREW_INSTALL_SCRIPT_URL="https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh"
+
+## Functions ##
+function usage () {
+    echo "Usage: $0 install"
+}
+
+function check_if_brew_is_installed () {
+    if command -v brew > /dev/null 2>&1; then
+        return 0
+    else
+        return 1
+    fi
+}
+
+function install () {
+    if check_if_brew_is_installed; then
+        echo "Homebrew is already installed"
+    else
+        echo "Installing homebrew via curl bash: $HOMEBREW_INSTALL_SCRIPT_URL"
+        /bin/bash -c "$(curl -fsSL "$HOMEBREW_INSTALL_SCRIPT_URL")"   
+    fi
+}
+
+function bundle () {
+    if check_if_brew_is_installed; then
+        brew bundle
+    else
+        echo "ERROR: brew is not installed"
+        return 1
+    fi
+}
+
+## Main ##
+command="$1"
+
+if [ -z "$command" ]; then
+    echo "ERROR: Missing argument 'command'"
+    usage
+    exit 1
+fi
+
+case $command in
+    bundle)
+        bundle
+        ;;
+    install)
+        install
+        ;;
+    *)
+        echo "ERROR: Invalid command '$command'"
+        usage
+        exit 1
+        ;;
+esac
