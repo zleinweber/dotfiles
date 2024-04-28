6fe6ff82baf28282b82a5d65132788393602f4c3
Zach Leinweber <zleinweb@gmail.com>
2024-04-28 19:05:21 +0000

    Further refine generic neovim settings

--- stow/neovim/.config/nvim/lua/settings.lua
+++ stow/neovim/.config/nvim/lua/settings.lua
@@ -1,12 +1,17 @@
 -- settings.lua
 local opt = vim.opt
 
--- Example settings
-opt.number = true            -- Show line numbers
-opt.relativenumber = true    -- Show relative line numbers
-opt.clipboard = "unnamedplus" -- Use the system clipboard
-opt.tabstop = 4              -- Number of spaces tabs count for
-opt.shiftwidth = 4           -- Number of spaces to use for autoindent
-opt.expandtab = true         -- Use spaces instead of tabs
-opt.smartindent = true       -- Insert indents automatically
-opt.autoindent = true        -- Copy indent from current line when starting a new line
+opt.fileformat = 'unix'         -- Set file format to unix
+opt.encoding = 'utf-8'          -- Set encoding to utf-8
+
+opt.clipboard = "unnamedplus"   -- Use the system clipboard
+
+opt.number = true               -- Show line numbers
+opt.relativenumber = true       -- Show relative line numbers
+
+opt.autoindent = true           -- Copy indent from current line when starting a new line
+opt.expandtab = true            -- Use spaces instead of tabs
+opt.shiftwidth = 4              -- Number of spaces to use for autoindent
+opt.smartindent = true          -- Insert indents automatically
+opt.smarttab = true             -- Use shiftwidth for tabstop
+opt.tabstop = 4                 -- Number of spaces tabs count for
