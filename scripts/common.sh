445ea005123d90c51aca7b71cf5adf7ac3476ef6
Zach Leinweber <zleinweb@gmail.com>
2024-10-13 02:19:13 +0000

    More consistent logging in setup script

--- scripts/common.sh
+++ scripts/common.sh
@@ -29,8 +29,6 @@ function source_recipe () {
     local recipe_path="${RECIPE_DIR}/${recipe_name}"
 
     if [ -r "$recipe_path" ]; then
-        echo_info "Loading recipe '$recipe_name'"
-
         # shellcheck source=/dev/null
         source "$recipe_path"
     else
@@ -39,16 +37,13 @@ function source_recipe () {
 }
 
 function sudo_up_front () {
-    # Useful primarily for macos where we have to sudo with password
+    # Useful primarily for MacOSX where we have to sudo with password
     if [ "$(id -u)" -ne 0 ]; then
-        echo_info "Requesting sudo password to cache credentials for script run duration"
         sudo -v
         while true; do
             sudo -n true
             sleep 60
             kill -0 "$$" || exit
         done 2>/dev/null &
-    else
-        echo_info "Already running as root"
     fi
 }
